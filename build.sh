ce8675bda16ffac4f47f4084006715077791596c
Luara Meissner <127454234+meissnerluara@users.noreply.github.com>
2025-04-03 16:45:46 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Instalar dependências
+pip install -r requirements.txt
+
+# Aplicar migrações do banco de dados
 python manage.py migrate
+
+# Coletar arquivos estáticos
 python manage.py collectstatic --noinput
