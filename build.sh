5611580db1dc59eaf973d177ddf561c19c01f34e
Luara Meissner <127454234+meissnerluara@users.noreply.github.com>
2025-04-03 16:48:03 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 pip install --upgrade pip
-pip install --force-reinstall Django
-
-# Instalar dependências
 pip install -r requirements.txt
 
-# Aplicar migrações do banco de dados
 python manage.py migrate
-
-# Coletar arquivos estáticos
 python manage.py collectstatic --noinput
